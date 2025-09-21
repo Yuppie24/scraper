a4c38c3bbf8cdae06660036862b894a331bcbafd
Yuppie <165232933+Yuppie24@users.noreply.github.com>
2025-09-21 16:02:36 +0545

    More commands

--- scrape.py
+++ scrape.py
@@ -9,6 +9,17 @@
 # git commit-m "Your message" => commit the changes
 #copy paste git code from github
 
+
+
+
+
+##############
+# 1. change the code
+# 2. git add .
+# 3. git commit -m "Your message"
+# 4. git push 
+##############
+
 import json
 import requests
 from bs4 import BeautifulSoup
